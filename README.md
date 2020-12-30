f29c3f20427b336eeb6effa6f8877b22318b164f
Josef pavlik <josef@pavlik.it>
2020-12-30 14:11:50 +0100

    added support for inputs

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ INSTALL:
 Clone the repository to ~/domoticz/plugins and restart domoticz
 
 SETUP:
-Go to the Hardware menu and create new hardware of type IOplus. Set the board id. 
+Go to the Hardware menu and create new hardware of type "Raspberry IOplus". Set the board id. 
 Devices will be created automatically.
+There are devices Relay_1 to Relay_8 and Input_1 to Input_8.
+Currently only relays and opto isolated inputs are supported.
+
 
--- plugin.py
+++ plugin.py
@@ -1,20 +1,29 @@
 # IOplus
 #
-# Author: Jet 2020
-# based on HTML.py example
+# Author: Josef Pavlik, 2020
 #
 #
 """
-<plugin key="IOplus" name="Raspberry IOplus" author="Jet" version="0.1" externallink="">
+<plugin key="IOplus" name="Raspberry IOplus" author="Josef Pavlik" version="0.1" externallink="">
     <description>
     IOplus 8 relay board on raspberry
     </description>
     <params>
         <param field="Mode1" label="board number" width="75px" default="0"/>
-        <param field="Mode2" label="debug" width="75px" default="0"/>
+        <param field="Mode6" label="Debug" width="75px">
+          <options>
+              <option label="True" value="Debug"/>
+              <option label="False" value="Normal"  default="true" />
+          </options>
+      </param>
+
     </params>
 </plugin>
 """
+
+# units 1-8 are  relays
+# units 9-16 are inputs
+
 import Domoticz
 import os
 import subprocess
@@ -41,11 +50,11 @@ class BasePlugin:
       
     def onStart(self):        
         self.board=int(Parameters["Mode1"])
-        self.debug=int(Parameters["Mode2"])
-        Domoticz.Log("onStart - Plugin is starting.")
-        for x in range(len(Devices), 8):
-            Domoticz.Device(Name="Relay_"+str(x+1), Unit=x+1, TypeName="Switch", Used=1).Create()
-        for x in range(1, 8):
+        self.debug=Parameters["Mode6"]=="Debug"
+        Domoticz.Log("onStart - Plugin is starting, debug="+str(self.debug))
+        for x in range(1, 9):
+            if (x not in Devices): Domoticz.Device(Name="Relay_"+str(x), Unit=x, TypeName="Switch", Used=1).Create()
+            if (x+8 not in Devices): Domoticz.Device(Name="Input_"+str(x), Unit=x+8, TypeName="Switch", Used=1).Create()
             self.set_relay(x, Devices[x].nValue)
         Domoticz.Heartbeat(3)
         self.running=1
@@ -61,9 +70,10 @@ class BasePlugin:
         return
       
     def onCommand(self, Unit, Command, Level, Hue):
-        self.set_relay(Unit, Command=="On")
         val=Command=="On" and "1" or "0"
-        if (Unit in Devices): Devices[Unit].Update(int(val),val)
+        if (Unit in range(1,9)): 
+          self.set_relay(Unit, Command=="On")
+          Devices[Unit].Update(int(val),val)
 
     def onDisconnect(self, Connection):
         unused=0
@@ -71,10 +81,13 @@ class BasePlugin:
     def onHeartbeat(self):
         if self.running:
           val=ioplus.getRelays(self.board)
-          if (self.debug): Domoticz.Log("read relays -> %02x" % val)
-          for Unit in range(1,8):
+          inp=ioplus.getOpto(self.board)
+          if (self.debug): Domoticz.Log("read relays -> %02x, inputs -> %02x" % val,inp)
+          for Unit in range(1,9):
             val1=(val >> (Unit-1)) & 1
+            inp1=(inp >> (Unit-1)) & 1
             if (Unit in Devices): Devices[Unit].Update(int(val1),str(val1))
+            if (Unit+8 in Devices): Devices[Unit+8].Update(int(inp1),str(val1))
             
 #            command="megaio "+self.board+" rread "+str(Unit)
 #            val=os.popen(command).read()
